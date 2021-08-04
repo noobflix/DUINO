43d61e6f0c3f941f9ce6d3f710cfb1a7249006f0
noobflix <64874770+noobflix@users.noreply.github.com>
2021-08-03 22:25:10 -0300

    Add files via upload

---
+++ duino-coin-inslador termux.sh
@@ -0,0 +1,9 @@
+#SEJA BEM VINDO AO ALTOMATIZADOR VAMOS FICAR RICO 2.0
+
+apt  update -y
+apt upgrade -y
+apt install wget -y
+pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
+
+./start-ubuntu.sh
+
